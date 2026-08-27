3c474c4a6f7172fd9627a5e66c037e1b5ecd52ee
DoctorSukhera <doctor.sukhera@gmail.com>
2026-08-28 01:01:15 +0800

    Delete test_import.py

--- test_import.py
+++
@@ -1,6 +0,0 @@
-def test_imports():
-    import nanomax_editor.api
-    import nanomax_editor.document_io
-    import nanomax_editor.exporters
-    import nanomax_editor.workflow
-    assert nanomax_editor.__version__ == "7.0"
